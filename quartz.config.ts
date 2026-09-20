26bd1a8ff53ec7edd48261b6f72ed27de98e9ed3
fyxss <mdtarekhasanbhuiyan@gmail.com>
2026-09-20 23:46:14 +0600

    chore(config): set baseUrl to fyxss.github.io/batch-notes

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "localhost:8080",
+    baseUrl: "fyxss.github.io/batch-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
